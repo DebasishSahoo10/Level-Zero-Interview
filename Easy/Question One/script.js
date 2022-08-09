4f841861938442264ab74c479a2b1524416959c8
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-08-09 12:43:56 +0530

    solution : added all codes

---
+++ Easy/Question One/script.js
@@ -0,0 +1,24 @@
+// Build a counter application and then some questions were asked related to the coding part
+
+var counterHolder = document.querySelector("#counter-holder")
+var increaseBtn = document.querySelector("#increase-btn")
+var decreaseBtn = document.querySelector("#decrease-btn")
+
+
+var counterValue = 0;
+
+var increaseCounter = () => {
+    counterValue = counterValue + 1
+    counterHolder.innerHTML = (counterValue)
+}
+
+var decreaseCounter = () => {
+    counterValue = counterValue - 1
+    counterHolder.innerHTML = (counterValue)
+}
+
+
+
+
+increaseBtn.addEventListener("click", increaseCounter)
+decreaseBtn.addEventListener("click", decreaseCounter)
