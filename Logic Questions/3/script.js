a57ae083aaa7b3f6c504f4d9b8ce4d8744274045
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-10-28 15:08:43 +0530

    doc : added new question and solution

---
+++ Logic Questions/3/script.js
@@ -0,0 +1,18 @@
+// Write a program to take a number input from user and determine whether the number is odd or even.
+
+
+var testNumber1 = 456
+var testNumber2 = 45
+var testNumber3 = 4
+
+
+
+var checkOddEven = () => {
+  if (testNumber1 % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+checkOddEven()
