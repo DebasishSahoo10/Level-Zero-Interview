e965bc84fef78c45b45cf6545f7622d7e6cfee7a
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-10-28 17:51:34 +0530

    refactor : added new question

---
+++ Logic Questions/7.js
@@ -0,0 +1,21 @@
+// Write a program to take a number input from user and print multiplication table 12 times for that number.
+
+
+var array = []
+
+
+for (let i=1; i<13; i++) {
+  var userInputNum = 5
+  userInputNum = userInputNum * i
+  array.push(userInputNum)
+}
+console.log(array)
+
+
+// a simpler solution will be
+
+var userNum = 5
+
+for (let i=1; i<13; i++) {
+  console.log(`${userNum} * ${i} = ${userNum * i}`) 
+}
