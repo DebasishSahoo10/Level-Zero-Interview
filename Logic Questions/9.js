a70f0e15218a503d268e1ca2de238de171a4fb43
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-10-28 20:55:54 +0530

    refactor : added new question

---
+++ Logic Questions/9.js
@@ -0,0 +1,11 @@
+// Write a program to take an input from a user and find its Factorial. Example: Factorial of 5 is 120
+
+
+var input = 7;
+var n = input 
+
+for (let i = 1; i<n; i++) {
+  input = input * (n-i)
+}
+
+console.log(input)
