c83f466b2113948a9255cb02df1f6ba043c2c07e
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-01 12:12:31 +0530

    refactor : added a new question

---
+++ Logic Questions/14.js
@@ -0,0 +1,9 @@
+// Given an array and an item, your function should return the index at which the item is present.
+// Example:
+// Input: indexOf([1,6,3,5,8,9], 3) ––> Output: 2
+
+
+
+var array = [1,6,3,5,8,9]
+
+console.log(array.indexOf(3))
