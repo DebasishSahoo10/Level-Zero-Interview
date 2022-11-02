148c1521cdfefda7e1cbe1e59269d34a954096f4
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-02 12:39:14 +0530

    refactor : added new question

--- Logic Questions/numbers.js
+++ Logic Questions/numbers.js
@@ -1,8 +1,6 @@
 // Write a program to calculate sum of N natural digits, as input by the users?
 // Enter a positive integer: 100
 // Sum = 5050
-
-
 var sumOfIntegers = (x) => {
     var sum = 0
     for (let i=0; i<=x; i++) {
@@ -15,11 +13,10 @@ var sumOfIntegers = (x) => {
 
 
 
-  
+
 // Write a Program to Print N Odd Number in Descending Order.
 // Input : 4
 // Output : 7 5 3 1
-
 var printOddNums = (x) => {
     for (let i=1; i<=(x+x); i++) {
       if (i%2 != 0) {
@@ -29,3 +26,18 @@ var printOddNums = (x) => {
   }
   
   printOddNums(7)
+
+
+// Write a JavaScript program to compute the sum of all digits that occur in a given string.
+// Input: 1234
+// Output: 1+2+3+4 = 10
+var sumOfStringChars = (n) => {
+    var chars = n.split("")
+    var sum = 0
+    for (let i=0; i<chars.length; i++) {
+      sum = sum + Number(chars[i])
+    }
+    console.log(sum)
+  }
+  
+  sumOfStringChars("1234")
