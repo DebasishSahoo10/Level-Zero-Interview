25f91bd534bfd7722c4c73c725973ea99fcf21ef
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-02 13:02:01 +0530

    refactor : added new questions

--- Logic Questions/numbers.js
+++ Logic Questions/numbers.js
@@ -41,3 +41,22 @@ var sumOfStringChars = (n) => {
   }
   
   sumOfStringChars("1234")
+
+
+
+// TO BE DONE - 1
+// Write a Program to cyclically Rotate a Number by X positions in the left direction, as provided by the user.
+// Example-
+// Enter a Number : 1234
+// Enter the Number of Rotations : 2
+// Output : 3412
+
+// TO BE DONE - 2
+// Write a Program to convert Decimal to Binary.
+// Enter the number to convert: 5
+// Binary of Given Number is = 101
+
+// TO BE DONE - 3
+// Follow up Question : Write a Program to Convert Octal to Decimal.
+// Enter an octal number: 116
+// Octal of Given Number = 78 in decimal
