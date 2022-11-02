2b77767cd15f20ef6ed5d620802d4821916b89de
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-02 12:34:34 +0530

    refactor : added new question

--- Logic Questions/numbers.js
+++ Logic Questions/numbers.js
@@ -12,3 +12,20 @@ var sumOfIntegers = (x) => {
   }
   
   sumOfIntegers(100)
+
+
+
+  
+// Write a Program to Print N Odd Number in Descending Order.
+// Input : 4
+// Output : 7 5 3 1
+
+var printOddNums = (x) => {
+    for (let i=1; i<=(x+x); i++) {
+      if (i%2 != 0) {
+        console.log(i)
+      }
+    }
+  }
+  
+  printOddNums(7)
