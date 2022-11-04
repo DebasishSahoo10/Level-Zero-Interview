f020cd0e15970ac6ad30dafcb5aebadc146e0d42
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-04 14:26:59 +0530

    refactor : added new question

--- Logic Questions/string.js
+++ Logic Questions/string.js
@@ -1,6 +1,5 @@
 // Given a string "how was your day?" and a word "how", write a program that removes the occurrence of the specified word from given sentence. ( input: string⇒"programming camp are amazing",word⇒ "programming"; output:" camp are amazing")
 
-
 var removeString = (string, n) => {
     var array = string.split(" ")
     for (let i=0; i<array.length; i++) {
@@ -13,3 +12,22 @@ var removeString = (string, n) => {
   }
   
   removeString("programming camp are amazing for programming", "programming")
+
+
+
+  
+// Write a program to toggle case of each character of the string "good afternoon" (example: "neogcamp" ⇒ "nEoGcAmP" )
+
+var toggleCase = (string) => {
+    var array = string.split("")
+    for(let i=1; i<array.length; i=i+2) {
+      if (array[i] == array[i].toUpperCase()) {
+        array[i] = array[i].toLowerCase()
+      } else {
+        array[i] = array[i].toUpperCase()
+      }
+    }
+    console.log(array.join(""))
+  }
+  
+  toggleCase("neogcamp")
