96c56cee5381ac4d3ad2d99f34511af8bc1077a4
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-08 12:52:01 +0530

    refactor : added new question

--- Logic Questions/string.js
+++ Logic Questions/string.js
@@ -53,3 +53,15 @@ var getRepetitionChar = (n) => {
 }
 
 getRepetitionChar("accessibility")
+
+
+
+// Reverse the given string word wise. That is, the last word in given string should come at 1st place, last second word at 2nd place and so on. Individual words should remain as it is. example: Input : Welcome to NeoG Camp → Camp NeoG to Welcome
+
+
+var reverseSentence = (string) => {
+  string = string.split(" ").reverse().join(" ")
+  console.log(string)
+}
+
+reverseSentence("Welcome to NeoG Camp")
