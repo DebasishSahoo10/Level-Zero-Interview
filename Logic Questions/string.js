ddb649262a9ecc6c701bdbc28d8e735b209b17dc
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-09 15:35:35 +0530

    refactor : added new question

--- Logic Questions/string.js
+++ Logic Questions/string.js
@@ -126,3 +126,14 @@ var maskString = (string) => {
 }
 
 maskString("5565534276455423")
+
+
+
+// Write a program that removes the time from the given date string "Wed April 15, 7pm". It should return. only the date without the time.
+
+var extractDate = (string) => {
+  string = string.split(",")
+  console.log(string[0])
+}
+
+extractDate("Wed April 15, 7pm")
