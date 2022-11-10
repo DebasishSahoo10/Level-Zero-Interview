6501d242ab4d0a90fbd13eca788a50dd8c682a6f
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-10 13:30:50 +0530

    refactor : added new question

--- Logic Questions/arrays.js
+++ Logic Questions/arrays.js
@@ -9,3 +9,16 @@ var sumOfArray = (array) => {
   }
   
 sumOfArray([10,4,5,2,5,6,9])
+
+
+// Find average of an array and display the output.
+
+var sumOfArray = (array) => {
+    var sum = 0
+    for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i]
+    }
+    console.log(sum / array.length)
+  }
+  
+sumOfArray([10, 4, 5, 2, 5, 6, 9])
