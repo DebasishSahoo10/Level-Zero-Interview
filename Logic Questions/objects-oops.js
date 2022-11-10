dd7cc1a3c1fd9cd0a71e5367d9f69bdcf56dd463
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-11-11 03:00:48 +0530

    refactor : added database and new question

---
+++ Logic Questions/objects-oops.js
@@ -0,0 +1,55 @@
+const studentDetails = [
+	{
+		roll: "1",
+		name: "Rohan Singh",
+		maths: 86,
+		science: 90,
+		english: 75,
+		computer: 85
+	},
+	{
+		roll: "2",
+		name: "Ritvik Patel",
+		maths: 27,
+		science: 30,
+		english: 35,
+		computer: 30
+	},
+	{
+		roll: "3",
+		name: "Neha Maurya",
+		maths: 75,
+		science: 69,
+		english: 40,
+		computer: 75
+	},
+	{
+		roll: "4",
+		name: "Mohit Verma",
+		maths: 21,
+		science: 31,
+		english: 45,
+		computer: 40
+	},
+	{
+		roll: "5",
+		name: "Karan Trivedi",
+		maths: 70,
+		science: 80,
+		english: 35,
+		computer: 60
+	}
+]
+
+
+
+// Print the name and total marks of each student.
+
+var getNameAndMarks = (studentData) => {
+  for (let i=0; i<studentData.length; i++) {
+    var totalMark = studentData[i].maths + studentData[i].science + studentData[i].english + studentData[i].computer
+    console.log(`${studentData[i].name} : ${totalMark}`)
+  }
+}
+
+getNameAndMarks(studentDetails)
